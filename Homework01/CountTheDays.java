038263b6288f8cc1825838b0c7bec065bae59540
sofiaruiz <35272489+sofiaruiz@users.noreply.github.com>
2018-01-25 09:42:16 -0800

    Update CountTheDays.java

--- Homework01/CountTheDays.java
+++ Homework01/CountTheDays.java
@@ -4,6 +4,6 @@ public class CountTheDays {
 		for (int i = 0; i < 6; i++){
 			params[i] = Long.parseLong(args[i]);
 		}
-		System.out.println("days" + CalendarStuff.daysBetween(params[0], params[1], params[2], params[3], params[4], params[5]));
+		System.out.println("Days" + CalendarStuff.daysBetween(params[0], params[1], params[2], params[3], params[4], params[5]));
 	}
 }
